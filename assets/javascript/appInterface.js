b32de635cb41559396c2d811521cbcb03fca89d6
Tetsukabe <Conmos13@gmail.com>
2018-09-18 21:09:33 -0400

    Uploading for the day

--- assets/javascript/appInterface.js
+++ assets/javascript/appInterface.js
@@ -23,81 +23,81 @@ $(document).ready(function () {
   function renderChecklist() {
     function renderTodos(list) {
       $("#to-dos").empty(); // This will empty the html
-    
+
       // Creating a for loop so that we can dynamically create our todos onto the page
       for (var i = 0; i < list.length; i++) {
-        
+
         // Create a new variable that will hold a <p> tag.
         // Set the value of the list at "i" as text to the "<p>" element.
         var p = $("<p id='p-color'>");
         p.text(list[i]);
-        
+
         // Create a button based off of the number at i, which will cause it to be unique at every iteration.
         // Give button an attribute of "data-to-do" at i and a class called "checkbox".
         // Add a checkmark as the text of the button.
         var btnCheck = $("<button>");
         btnCheck
-        .attr("data-to-do", i)
-        .addClass("btn btn-checkbox checkbox")
-        .text("✓");
-    
+          .attr("data-to-do", i)
+          .addClass("btn btn-checkbox checkbox")
+          .text("✓");
+
         // Append the button to the todo item
         p = p.prepend(btnCheck);
-    
+
         // Add the button and the p tag to the todos div 
         $("#to-dos").append(p);
       }
     }
-    
-    $("#add-to-do").on("click", function(event) {
+
+    $("#add-to-do").on("click", function (event) {
       event.preventDefault();
-    
+
       // Obtain value from textbox and store it as a variable
       var toDoTask = $("#to-do").val().trim();
-    
+
       // Add new todo to localStorage
       list.push(toDoTask);
-    
+
       // Update ToDo's
       renderTodos(list);
-    
+
       // Save the todos into local storage.
       // We need JSON.stringify to convert them into a string inside the storage.
       localStorage.setItem("todoList", JSON.stringify(list));
-    
+
       // Clear the textbox when finished.
       $("#to-do").val("");
     });
-    
+
     // When user clicks the checkbox, that respective thing will disappear 
-    $(document).on("click", ".checkbox", function() {
-    
+    $(document).on("click", ".checkbox", function () {
+
       // Get the unique number of the specified button and hold it in a variable.
       var toDeleteNumber = $(this).attr("data-to-do");
-    
+
       // Deletes specified ID number of button
       list.splice(toDeleteNumber, 1);
-    
+
       // Update the todos on the page.
       renderTodos(list);
-    
+
       // Save the todos into local storage
       // JSON.stringify again
-    
+
       localStorage.setItem("todoList", JSON.stringify(list));
     });
-    
+
     // Load the todos from the localStorage so if it's a returning user, it shall display
     // JSON.parse is needed to re-string the array that gets received.
     var list = JSON.parse(localStorage.getItem("todoList"));
-    
+
     // Checks to see if todolist exists in localStorage and is already an array. 
     // If not, then set the array to empty.
     // Otherwise, our list of arrays shows
     if (!Array.isArray(list)) {
       list = [];
     }
-    
+
     // Update the todos!!!
     renderTodos(list);
   }
@@ -233,23 +233,169 @@ $(document).ready(function () {
   $("#btnNYT").on("click", function () {
 
     // Clear container
-    $("#CONTAINERGOESHERE").empty();
+    $("#appContainer").empty();
 
-    var newYorkTimes = ("<div class='container'><div class='jumbotron' style='background-color: #20315A ; color: white;'><h1 class='text-center'><strong><i class='fa fa-newspaper-o'></i> New York Times Search</strong></h1></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-list-alt'></i> Search Parameters</strong></div><div class='card-body'><form role='form'><div class='form-group'><label for='search'>Search Term:</label><input type='text' class='form-control' id='search-term'></div><div class='form-group'><label for='pwd'>Number of Records to Retrieve:</label><select id='article-count' class='custom-select' aria-labelledby='dropdownMenuButton'><option selected value='1'>1</option><option value='5' selected>5</option><option value='10'>10</option></select></div><div class='form-group'><label for='start-year'>Start Year (Optional):</label><input type='text' class='form-control' id='start-year'></div><div class='form-group'><label for='end-year'>End Year (Optional):</label><input type='text' class='form-control' id='end-year'></div><button type='submit' class='btn btn-default' id='run-search'><i class='fa fa-search'></i> Search</button><button class='btn btn-default' id='clear-all'><i class='fa fa-trash'></i> Clear Results</button></form></div></div></div></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-table'></i> Top Articles</strong></div><div class='card-body' id='article-section'></div></div></div></div><div class='row'><div class='col-sm-12'><hr><h5 class='text-center'><small>Made with lots and lots of<i class='fa fa-heart'></i></small></h5></div></div></div>")
+    var newYorkTimes = ("<div class='container NYTimes'><div class='jumbotron' style='background-color: #20315A ; color: white;'><h1 class='text-center'><strong><i class='fa fa-newspaper-o'></i> New York Times Search</strong></h1></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-list-alt'></i> Search Parameters</strong></div><div class='card-body'><form role='form'><div class='form-group'><label for='search'>Search Term:</label><input type='text' class='form-control' id='search-term'></div><div class='form-group'><label for='pwd'>Number of Records to Retrieve:</label><select id='article-count' class='custom-select' aria-labelledby='dropdownMenuButton'><option selected value='1'>1</option><option value='5' selected>5</option><option value='10'>10</option></select></div><div class='form-group'><label for='start-year'>Start Year (Optional):</label><input type='text' class='form-control' id='start-year'></div><div class='form-group'><label for='end-year'>End Year (Optional):</label><input type='text' class='form-control' id='end-year'></div><button type='submit' class='btn btn-default' id='run-search'><i class='fa fa-search'></i> Search</button><button class='btn btn-default' id='clear-all'><i class='fa fa-trash'></i> Clear Results</button></form></div></div></div></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-table'></i> Top Articles</strong></div><div class='card-body' id='article-section'></div></div></div></div><div class='row'><div class='col-sm-12'><hr><h5 class='text-center'><small>Made with lots and lots of<i class='fa fa-heart'></i></small></h5></div></div></div>")
 
-    $("#test-container").append(newYorkTimes)
+    $("#appContainer").append(newYorkTimes)
+    renderNYTimes();
   })
   //  End NYTimes HTML
 
+  // Start NYTimes javaScript
 
-  $("#btnGiphy").on("click", function () {
+  function renderNYTimes() {
+    function buildQueryURL() {
+      /**
+      * pulls information from the form and build the query URL
+      * @returns {string} URL for NYT API based on form inputs
+      */
+      // queryURL is the url we'll use to query the API
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
-    // Clear container
-    $("#CONTAINERGOESHERE").empty();
+      // Begin building an object to contain our API call's query parameters
+      // Set the API key
+      var queryParams = { "api-key": "b9f91d369ff59547cd47b931d8cbc56b:0:74623931" };
 
-    var giphy = ("<div class='row'><div id='button-view' class='col-11'></div></div><div class='row'><div id='gif-view' class='col-8 img-fluid'></div><div class='col-4'><form id='animal-form'><label for='animal-input'><strong>Add an animal, Yo!</strong></label><br><input style='width:350px;' type='text' id='animal-input'><br><input style='margin-top: 10px;' id='add-animal' type='submit' value='Submit' class='btn btn-info'><br><input style='margin-top: 10px;' id='load-more' type='submit' value='Load More' class='btn btn-info'></form></div></div><p class='totop'><a href='#top'>Back to top</a></p>")
-    $("#test-container").append(giphy);
-  });
+      // Grab text the user typed into the search input, add to the queryParams object
+      queryParams.q = $("#search-term")
+        .val()
+        .trim();
+
+      // If the user provides a startYear, include it in the queryParams object
+      var startYear = $("#start-year")
+        .val()
+        .trim();
+
+      if (parseInt(startYear)) {
+        queryParams.begin_date = startYear + "0101";
+      }
+
+      // If the user provides an endYear, include it in the queryParams object
+      var endYear = $("#end-year")
+        .val()
+        .trim();
+
+      if (parseInt(endYear)) {
+        queryParams.end_date = endYear + "0101";
+      }
+
+      // Logging the URL so we have access to it for troubleshooting
+      console.log("---------------\nURL: " + queryURL + "\n---------------");
+      console.log(queryURL + $.param(queryParams));
+      return queryURL + $.param(queryParams);
+    }
+
+    /**
+     * takes API data (JSON/object) and turns it into elements on the page
+     * @param {object} NYTData - object containing NYT API data
+     */
+    function updatePage(NYTData) {
+      // Get from the form the number of results to display
+      // API doesn't have a "limit" parameter, so we have to do this ourselves
+      var numArticles = $("#article-count").val();
+
+      // Log the NYTData to console, where it will show up as an object
+      console.log(NYTData);
+      console.log("------------------------------------");
+
+      // Loop through and build elements for the defined number of articles
+      for (var i = 0; i < numArticles; i++) {
+        // Get specific article info for current index
+        var article = NYTData.response.docs[i];
+
+        // Increase the articleCount (track article # - starting at 1)
+        var articleCount = i + 1;
+
+        // Create the  list group to contain the articles and add the article content for each
+        var $articleList = $("<ul>");
+        $articleList.addClass("list-group");
+
+        // Add the newly created element to the DOM
+        $("#article-section").append($articleList);
+
+        // If the article has a headline, log and append to $articleList
+        var headline = article.headline;
+        var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+        if (headline && headline.main) {
+          console.log(headline.main);
+          $articleListItem.append(
+            "<span class='label label-primary'>" +
+            articleCount +
+            "</span>" +
+            "<strong> " +
+            headline.main +
+            "</strong>"
+          );
+        }
+
+        // If the article has a byline, log and append to $articleList
+        var byline = article.byline;
+
+        if (byline && byline.original) {
+          console.log(byline.original);
+          $articleListItem.append("<h5>" + byline.original + "</h5>");
+        }
+
+        // Log section, and append to document if exists
+        var section = article.section_name;
+        console.log(article.section_name);
+        if (section) {
+          $articleListItem.append("<h5>Section: " + section + "</h5>");
+        }
+
+        // Log published date, and append to document if exists
+        var pubDate = article.pub_date;
+        console.log(article.pub_date);
+        if (pubDate) {
+          $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+        }
+
+        // Append and log url
+        $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+        console.log(article.web_url);
+
+        // Append the article
+        $articleList.append($articleListItem);
+      }
+    }
+
+    // Function to empty out the articles
+    function clear() {
+      $("#article-section").empty();
+    }
+
+    // CLICK HANDLERS
+    // ==========================================================
+
+    // .on("click") function associated with the Search Button
+    $("#run-search").on("click", function (event) {
+      // This line allows us to take advantage of the HTML "submit" property
+      // This way we can hit enter on the keyboard and it registers the search
+      // (in addition to clicks). Prevents the page from reloading on form submit.
+      event.preventDefault();
+
+      // Empty the region associated with the articles
+      clear();
+
+      // Build the query URL for the ajax request to the NYT API
+      var queryURL = buildQueryURL();
+
+      // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+      // The data then gets passed as an argument to the updatePage function
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(updatePage);
+    });
+
+    //  .on("click") function associated with the clear button
+    $("#clear-all").on("click", clear);
+
+  }
+
+  // End NYTimes javaScipt
 
   // Start HMTL for Giphy
 
