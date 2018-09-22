574f38b23cdf36073390ec4caad06dceecc2b8d5
Tetsukabe <Conmos13@gmail.com>
2018-09-22 11:46:53 -0400

    Pushing fixed NYTImes

--- assets/javascript/appInterface.js
+++ assets/javascript/appInterface.js
@@ -3,17 +3,17 @@
 $(document).ready(function () {
   // Begin homepage HTML
 
-    // Begin jQuery Calc HTML
-    $(document).on("click", "#btnHome", function () {
+  // Begin jQuery Calc HTML
+  $(document).on("click", "#btnHome", function () {
 
-      // Clear container
-      $("#appContainer").empty();
-  
-      // importing HTML for Calculator
-      var home = ("<div class='row'><img style='margin-left:auto;margin-right:auto' src='assets/image/bw.jpeg' alt='homepage'class='img-fluid col-12'></div>")
-    
-      $("#appContainer").append(home);
-    })
+    // Clear container
+    $("#appContainer").empty();
+
+    // importing HTML for Calculator
+    var home = ("<div class='row'><img style='margin-left:auto;margin-right:auto' src='assets/image/bw.jpeg' alt='homepage'class='img-fluid col-12'></div>")
+
+    $("#appContainer").append(home);
+  })
 
   // End homepage HTML
 
@@ -25,11 +25,11 @@ $(document).ready(function () {
     $("#appContainer").empty();
 
     // importing HTML for Calculator
-    var checklist = ("<div class='row'>"+
-    "<div class='container'><h1 class='display-4 title-display'> Checklist </h1>"+
-    "<p class='lead lead-text'>This is a simple checklist that you'll be able to use on the go. Unfortunately, it  doesn't have cloud storage as of yet, but it does have local!</p>"+
-    "<form><span id='todo-items'>To Do Items: <input class='form-control col-12' id='to-do' type='text'><input style='margin-top:10px;' class='btn btn-light' id='add-to-do' value='Add Item' type='submit'></span></form><br>"+
-    "<div id='to-dos'></div></div></div>")
+    var checklist = ("<div class='row'>" +
+      "<div class='container'><h1 class='display-4 title-display'> Checklist </h1>" +
+      "<p class='lead lead-text'>This is a simple checklist that you'll be able to use on the go. Unfortunately, it  doesn't have cloud storage as of yet, but it does have local!</p>" +
+      "<form><span id='todo-items'>To Do Items: <input class='form-control col-12' id='to-do' type='text'><input style='margin-top:10px;' class='btn btn-light' id='add-to-do' value='Add Item' type='submit'></span></form><br>" +
+      "<div id='to-dos'></div></div></div>")
 
 
     $("#appContainer").append(checklist);
@@ -254,15 +254,169 @@ $(document).ready(function () {
   $("#btnNYT").on("click", function () {
 
     // Clear container
-    $("#appContent").empty();
+    $("#appContainer").empty();
 
     var newYorkTimes = ("<div class='container'><div class='jumbotron' style='background-color: #20315A ; color: white;'><h1 class='text-center'><strong><i class='fa fa-newspaper-o'></i> New York Times Search</strong></h1></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-list-alt'></i> Search Parameters</strong></div><div class='card-body'><form role='form'><div class='form-group'><label for='search'>Search Term:</label><input type='text' class='form-control' id='search-term'></div><div class='form-group'><label for='pwd'>Number of Records to Retrieve:</label><select id='article-count' class='custom-select' aria-labelledby='dropdownMenuButton'><option selected value='1'>1</option><option value='5' selected>5</option><option value='10'>10</option></select></div><div class='form-group'><label for='start-year'>Start Year (Optional):</label><input type='text' class='form-control' id='start-year'></div><div class='form-group'><label for='end-year'>End Year (Optional):</label><input type='text' class='form-control' id='end-year'></div><button type='submit' class='btn btn-default' id='run-search'><i class='fa fa-search'></i> Search</button><button class='btn btn-default' id='clear-all'><i class='fa fa-trash'></i> Clear Results</button></form></div></div></div></div><div class='row'><div class='col-sm-12'><br><div class='card'><div class='card-header'><strong><i class='fa fa-table'></i> Top Articles</strong></div><div class='card-body' id='article-section'></div></div></div></div><div class='row'><div class='col-sm-12'><hr><h5 class='text-center'><small>Made with lots and lots of<i class='fa fa-heart'></i></small></h5></div></div></div>")
 
-    $("#test-container").append(newYorkTimes)
+    $("#appContainer").append(newYorkTimes)
+    renderNYTimes();
   })
   //  End NYTimes HTML
 
+  // Being NYTimes Java
 
+  function renderNYTimes() {
+    /**
+* pulls information from the form and build the query URL
+* @returns {string} URL for NYT API based on form inputs
+*/
+    function buildQueryURL() {
+      // queryURL is the url we'll use to query the API
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+      // Begin building an object to contain our API call's query parameters
+      // Set the API key
+      var queryParams = { "api-key": "b9f91d369ff59547cd47b931d8cbc56b:0:74623931" };
+
+      // Grab text the user typed into the search input, add to the queryParams object
+      queryParams.q = $("#search-term")
+        .val()
+        .trim();
+
+      // If the user provides a startYear, include it in the queryParams object
+      var startYear = $("#start-year")
+        .val()
+        .trim();
+
+      if (parseInt(startYear)) {
+        queryParams.begin_date = startYear + "0101";
+      }
+
+      // If the user provides an endYear, include it in the queryParams object
+      var endYear = $("#end-year")
+        .val()
+        .trim();
+
+      if (parseInt(endYear)) {
+        queryParams.end_date = endYear + "0101";
+      }
+
+      // Logging the URL so we have access to it for troubleshooting
+      console.log("---------------\nURL: " + queryURL + "\n---------------");
+      console.log(queryURL + $.param(queryParams));
+      return queryURL + $.param(queryParams);
+    }
+
+    /**
+     * takes API data (JSON/object) and turns it into elements on the page
+     * @param {object} NYTData - object containing NYT API data
+     */
+    function updatePage(NYTData) {
+      // Get from the form the number of results to display
+      // API doesn't have a "limit" parameter, so we have to do this ourselves
+      var numArticles = $("#article-count").val();
+
+      // Log the NYTData to console, where it will show up as an object
+      console.log(NYTData);
+      console.log("------------------------------------");
+
+      // Loop through and build elements for the defined number of articles
+      for (var i = 0; i < numArticles; i++) {
+        // Get specific article info for current index
+        var article = NYTData.response.docs[i];
+
+        // Increase the articleCount (track article # - starting at 1)
+        var articleCount = i + 1;
+
+        // Create the  list group to contain the articles and add the article content for each
+        var $articleList = $("<ul>");
+        $articleList.addClass("list-group");
+
+        // Add the newly created element to the DOM
+        $("#article-section").append($articleList);
+
+        // If the article has a headline, log and append to $articleList
+        var headline = article.headline;
+        var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+        if (headline && headline.main) {
+          console.log(headline.main);
+          $articleListItem.append(
+            "<span class='label label-primary'>" +
+            articleCount +
+            "</span>" +
+            "<strong> " +
+            headline.main +
+            "</strong>"
+          );
+        }
+
+        // If the article has a byline, log and append to $articleList
+        var byline = article.byline;
+
+        if (byline && byline.original) {
+          console.log(byline.original);
+          $articleListItem.append("<h5>" + byline.original + "</h5>");
+        }
+
+        // Log section, and append to document if exists
+        var section = article.section_name;
+        console.log(article.section_name);
+        if (section) {
+          $articleListItem.append("<h5>Section: " + section + "</h5>");
+        }
+
+        // Log published date, and append to document if exists
+        var pubDate = article.pub_date;
+        console.log(article.pub_date);
+        if (pubDate) {
+          $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+        }
+
+        // Append and log url
+        $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+        console.log(article.web_url);
+
+        // Append the article
+        $articleList.append($articleListItem);
+      }
+    }
+
+    // Function to empty out the articles
+    function clear() {
+      $("#article-section").empty();
+    }
+
+    // CLICK HANDLERS
+    // ==========================================================
+
+    // .on("click") function associated with the Search Button
+    $("#run-search").on("click", function (event) {
+      // This line allows us to take advantage of the HTML "submit" property
+      // This way we can hit enter on the keyboard and it registers the search
+      // (in addition to clicks). Prevents the page from reloading on form submit.
+      event.preventDefault();
+
+      // Empty the region associated with the articles
+      clear();
+
+      // Build the query URL for the ajax request to the NYT API
+      var queryURL = buildQueryURL();
+
+      // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+      // The data then gets passed as an argument to the updatePage function
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(updatePage);
+    });
+
+    //  .on("click") function associated with the clear button
+    $("#clear-all").on("click", clear);
+
+  }
+
+  // End NYtimes Java
 
 
   // Begin HMTL for Giphy
@@ -409,7 +563,7 @@ $(document).ready(function () {
             .attr("src", results[i].images.fixed_height_still.url)
             .attr("data-still", results[i].images.fixed_height_still.url)
             .attr("data-animate", results[i].images.fixed_height.url)
-   
+
 
 
           // Appending the image and then the paragraph to the gifDiv.
@@ -418,15 +572,15 @@ $(document).ready(function () {
 
           // Prepending the gifDiv to the #gif-div
           $("#gif-div").prepend(gifDiv);
-          
-          
+
+
         }
-        
+
         attachListeners()
 
 
       });
-      
+
 
       function attachListeners() {
         $(".gif").on("click", function () {
